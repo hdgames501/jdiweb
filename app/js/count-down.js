14ee7f80a1492747e87c0246e740aa2ddfa7ef84
hdgames501 <112070632+hdgames501@users.noreply.github.com>
2022-08-27 17:47:12 +0530

    Add files via upload

---
+++ app/js/count-down.js
@@ -0,0 +1,28 @@
+// Set the date we're counting down to
+var countDownDate = new ("August 27, 2022 20:30:00 GMT+0530").getTime();
+
+var x = setInterval(function () {
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("timer-d").innerHTML = days;
+  document.getElementById("timer-hr").innerHTML = hours;
+  document.getElementById("timer-m").innerHTML = minutes;
+  document.getElementById("timer-s").innerHTML = seconds;
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
